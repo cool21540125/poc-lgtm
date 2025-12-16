dd6f708004bf049a7ac49bf8e38a66c14452e736
Tony <tony@weibyapps.com>
2025-12-16 12:06:35 +0800

    fix otel logging

--- logging.js
+++ logging.js
@@ -2,19 +2,18 @@ const { LoggerProvider, SimpleLogRecordProcessor } = require('@opentelemetry/sdk
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
 const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } = require('@opentelemetry/semantic-conventions');
-const { logs, SeverityNumber } = require('@opentelemetry/api-logs');
+const { logs } = require('@opentelemetry/api-logs');
 
 const loggerProvider = new LoggerProvider({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: 'tony_auto',
     [ATTR_SERVICE_VERSION]: '0.1.0',
   }),
-  logRecordProcessors: [new SimpleLogRecordProcessor(new OTLPLogExporter({
+  processors: [new SimpleLogRecordProcessor(new OTLPLogExporter({
     url: 'http://localhost:4318/v1/logs',
   }))],
 });
 logs.setGlobalLoggerProvider(loggerProvider);
-const logger = loggerProvider.getLogger('log001', '0.1.0');
 
 process.on('SIGTERM', () => {
   loggerProvider.shutdown()
